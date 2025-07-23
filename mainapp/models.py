7c335e9100744445a0a365516d9746cdcb57c660
Souvik Sikder <141018000+Souvik1610@users.noreply.github.com>
2025-07-23 12:50:52 +0530

    Update models.py

--- mainapp/models.py
+++ mainapp/models.py
@@ -1,1 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+class CustomUser(AbstractUser):
+    USER_TYPE_CHOICES = (
+        ('student', 'Student'),
+        ('college', 'College'),
+        ('recruiter', 'Recruiter'),
+    )
+    user_type = models.CharField(max_length=20, choices=USER_TYPE_CHOICES)
+# Create your models here.
