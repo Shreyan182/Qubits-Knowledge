71738d3c3676501246f13541ac9dea220ecbb8c1
Souvik Sikder <141018000+Souvik1610@users.noreply.github.com>
2025-07-23 12:50:22 +0530

    Update urls.py

--- mainapp/urls.py
+++ mainapp/urls.py
@@ -1,15 +1,11 @@
-from django.contrib import admin
 from django.urls import path
 from core import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', views.login, name='login'),
-    path('login/', views.login, name='login'),
+    path('', views.user_login, name='login'),
+    path('login/', views.user_login, name='login'),
     path('logout/', views.logout, name='logout'),
-    path('register/', views.register, name='register_html'),
-    path('register/', views.register, name='register'),
-    path('home', views.home, name='home_html'),
+    path('register/', views.register_view, name='register'),
     path('home/', views.home, name='home'),
     path('about/', views.about, name='about'),
     path('contact/', views.contact, name='contact'),  
@@ -28,5 +24,3 @@ urlpatterns = [
     path('practice/', views.practice, name='practice'),
     path('boostyourknowledge/', views.boostyourknowledge, name='boostyourknowledge'),
 ]
-# This file defines the URL patterns for the core application of the Unstop clone project.
-# Each path corresponds to a view function that renders a specific template.
